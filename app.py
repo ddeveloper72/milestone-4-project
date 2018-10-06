9bfadf34ed41fd9f43e542460a574a331473bd76
ddeveloper72 <duncan.niamh@gmail.com>
2018-10-06 19:55:22 +0100

    Updated app = Flask(__name__) and corrected typo in app.run(debug=True)

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
 from flask import Flask
 
-app = Flask(__app__)
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World! This is my Milestone 4 Project Kick-Start'
 
+
 if __name__ == '__main__':
-    app.run(host.environ.get('IP'),
+    
+    # assign a port ID works with Vscode
+   
+    app.run(host=os.getenv('IP'),
+        port=os.getenv('PORT'),
+        # debug set to true to help during development
+        debug=True)
+
+""" if __name__ == '__main__':
+
+    # Cloud 9 Environmental variables
+
+    app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
-    debig=True)
+    # debug set to true to help during development
+    debug=True) """
