80c37b2d61ce72d7a10bda363bf362655a37930c
ddeveloper72 <duncan.niamh@gmail.com>
2018-10-08 22:47:13 +0100

    1st Test appointment run, to comare code schema to database schema.

--- app.py
+++ app.py
@@ -16,11 +16,30 @@ mongo = PyMongo(app)
 # Basebuild function
 # Search for scheduled appointments - no filters.
 @app.route('/')
-@app.route('/get_departmemts')
-def get_departments():
-    return render_template("departments.html",
+@app.route('/get_appointments')
+def get_appointments():
+    return render_template("appointments.html",
                             appointment = mongo.db.appointment.find())
 
+@app.route('/add_appointment')
+def add_appointment():
+    return render_template("add_appointment.html",
+                            departments = mongo.db.departments.find())
+
+
+@app.route('/insert_appointment',  methods=["POST"])
+def insert_appointment():
+    appointment = mongo.db.appointment
+    appointment.insert_one(request.form.to_dict())
+    return redirect(url_for('get_appointments'))
+
+
+""" @app.route('/edit_appointment/<appointment_id>')
+def edit_appointment(appointment_id):
+    _appointment = mongo.db.appointment.find_one({'_id': ObjectId(appointment_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editappointment.html', 
+                            appointment=_appointment, categories=all_categories) """
 
 if __name__ == '__main__':
     
