fcf1c6349527a880bf30d50d666cff1fe505c43f
ddeveloper72 <duncan.niamh@gmail.com>
2018-10-21 10:59:46 +0100

    Stripped back EVERYTHING to fix services selector jQuery

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,34 +1,34 @@
 //waits until page is loaded first
-$(document).ready(function () {
+/* $(document).ready(function () {
     $('.collapsible').collapsible();
     $('select').material_select();
     $('.button-collapse').sideNav();
     $('.datepicker').pickadate();
     $('.timepicker').pickatime();
-});
+}); */
 
 // Date picker:
-$('.datepicker').pickadate({
+/* $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
     today: 'Today',
     clear: 'Clear',
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
-});
+}); */
 
 // Credit to https://github.com/chingyawhao/materialize-clockpicker/
 // Time picker:
-$('.timepicker').pickatime({
+/* $('.timepicker').pickatime({
     default: 'now',
     twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
     close: 'OK',
     autoclose: false,
     vibrate: true // vibrate the device when dragging clock hand
-});
+}); */
 
 // select currently active depatrmtnt
-$("#department").change(function() {
+$("#departments").change(function() {
     let cur_value = $('option:selected', this).val();
     console.log(cur_value);
     $.ajax({
